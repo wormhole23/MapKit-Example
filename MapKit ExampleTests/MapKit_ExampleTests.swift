a5760ac0f0172128a48027fd9fffc89070baf654
Aziza Kamet <19636@204-17.local>
2016-11-15 13:33:43 +0600

    Initial Commit

---
+++ MapKit ExampleTests/MapKit_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  MapKit_ExampleTests.swift
+//  MapKit ExampleTests
+//
+//  Created by Aziza Kamet on 15.11.16.
+//  Copyright © 2016 Aziza Kamet. All rights reserved.
+//
+
+import XCTest
+@testable import MapKit_Example
+
+class MapKit_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
